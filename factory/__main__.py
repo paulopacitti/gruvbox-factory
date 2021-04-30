ff640cc3f814e425fd73ed07d4cf9679bafed072
Philipp Landmann <landmann.philipp@gmx.de>
2021-04-30 22:11:46 +0200

    added command line arguments option to supply image paths; images from outside the script's working directory are allowed now

--- factory/__main__.py
+++ factory/__main__.py
@@ -10,6 +10,7 @@ from rich.panel import Panel
 from rich.prompt import IntPrompt
 from rich.progress import track
 
+
 def main():
     signal.signal(signal.SIGINT, signal_handler)
     console = Console()
@@ -19,30 +20,58 @@ def main():
     gruvbox_factory.reset_palette()
     add_gruvbox_palette()
 
-    console.print(Panel('🏭 [bold green] Gruvbox Factory [/] 🏭', expand=False, border_style='yellow'))
-    console.print('⚠️ WARNING ⚠️\n[italic]make sure you\'re in the same directory of the image you want to convert [/]\n')
-    image_file = console.input('🖼️ [bold yellow]Which image do you want to manufacture?[/] ')
+    # Parse arguments / input as a list
+    if len(sys.argv) == 1:
+        console.print(
+            Panel(
+                "🏭 [bold green] Gruvbox Factory [/] 🏭",
+                expand=False,
+                border_style="yellow",
+            )
+        )
+        image_paths = console.input("🖼️ [bold yellow]Which image do you want to manufacture?[/]").split()
+    else:
+        image_paths = sys.argv[1:]
 
-    try:
-        image = gruvbox_factory.open_image(image_file)
-    except:
-        console.print('❌ [red]We had a problem in the pipeline! Make sure you\'re in the same path of the image you want to convert! [/]')
-        sys.exit(0)
-    console.print('🔨 [yellow]manufacturing your gruvbox wallpaper...[/]')
-    gruvbox_factory.convert_image(image, save_path=('gruvbox_' + image_file))
+    # Convert each image to gruvbox colors
+    for image_path in image_paths:
+        try:
+            image = gruvbox_factory.open_image(image_path)
+        except:
+            console.print(
+                f"❌ [red]We had a problem in the pipeline! Make sure your picure can be found at '{image_path}'! [/]"
+            )
+            sys.exit(1)
+        console.print(f"🔨 [yellow]manufacturing gruvbox wallpaper '{image_path}'...[/]")
+        gruvbox_factory.convert_image(
+            image,
+            save_path=(
+                os.path.join(
+                    os.path.dirname(image_path),
+                    "gruvbox_" + os.path.basename(image_path),
+                )
+            ),
+        )
 
-    console.print('✅ [bold green]Done![/] [green](saved as gruvbox_' + image_file + ')[/]')
+        console.print(
+            "✅ [bold green]Done![/] [green](saved as gruvbox_"
+            + os.path.basename(image_path)
+            + ")[/]"
+        )
+
 
 def add_gruvbox_palette():
     current_path = Path(__file__).parent.absolute()
-    palette = open(str(current_path) + '/gruvbox.txt', 'r')
+    palette = open(str(current_path) + "/gruvbox.txt", "r")
     for line in palette.readlines():
         gruvbox_factory.add_color_to_palette(line[:-1])
+
 
 ## handle CTRL + C
 def signal_handler(signal, frame):
     print()
     sys.exit(0)
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     main()
