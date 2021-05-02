8e55007952495cdc578986525a59b59e89e331ac
philipp <50250716+Kuuhhl@users.noreply.github.com>
2021-05-02 19:28:57 +0200

    command-line arguments option, supply multiple inputs at once, can convert images outside working directory (#7)
    * added command line arguments option to supply image paths; images from outside the script's working directory are allowed now
    
    * updated prompt message

--- factory/__main__.py
+++ factory/__main__.py
@@ -2,7 +2,6 @@ import signal
 import argparse
 import sys
 import os
-from pathlib import Path
 
 from ImageGoNord import GoNord
 
@@ -15,74 +14,84 @@ def main():
     signal.signal(signal.SIGINT, signal_handler)
     console = Console()
 
+    gruvbox_factory = GoNord()
+    gruvbox_factory.reset_palette()
+    add_gruvbox_palette(gruvbox_factory)
+
     # Checks if there's an argument
     if len(sys.argv) > 1:
-        image_file = fromCommandArgument(console)
+        image_paths = fromCommandArgument(console)
     else:
-        image_file = fromTui(console)
-    process_image(image_file, console)
+        image_paths = fromTui(console)
 
+    for image_path in image_paths:
+        if os.path.isfile(image_path):
+            process_image(image_path, console, gruvbox_factory)
+        else:
+            console.print(
+                f"❌ [red]We had a problem in the pipeline! \nThe image at '{image_path}' could not be found! \nSkipping... [/]"
+            )
+            continue
 
 
 # Gets the file path from the Argument
 def fromCommandArgument(console):
 
     command_parser = argparse.ArgumentParser(
-        description='A simple cli to manufacture a gruvbox themed wallpaper.'
+        description="A simple cli to manufacture gruvbox themed wallpapers."
     )
-    command_parser.add_argument('Path',
-                       metavar='path',
-                       type=str,
-                       help='The path to the image.')
+    command_parser.add_argument(
+        "Path", metavar="path", nargs="+", type=str, help="The path(s) to the image(s)."
+    )
     args = command_parser.parse_args()
 
-    if args.Path is None:
-        console.print('❌ [red]We had a problem in the pipeline! Make sure you\'re in the same path of the image you want to convert! [/]')
-        sys.exit(0)
-    else:
-        return args.Path
+    return args.Path
 
 
 # Gets the file path from user input
 def fromTui(console):
 
-    console.print(Panel('🏭 [bold green] Gruvbox Factory [/] 🏭', expand=False, border_style='yellow'))
-    console.print('⚠️ WARNING ⚠️\n[italic]make sure you\'re in the same directory of the image you want to convert [/]\n')
-    return console.input('🖼️ [bold yellow]Which image do you want to manufacture?[/] ')
+    console.print(
+        Panel(
+            "🏭 [bold green] Gruvbox Factory [/] 🏭", expand=False, border_style="yellow"
+        )
+    )
 
-def process_image(image_file, console):
-    gruvbox_factory = GoNord()
-    gruvbox_factory.reset_palette()
-    add_gruvbox_palette(gruvbox_factory)
-    try:
-        image = gruvbox_factory.open_image(image_file)
-    except:
-        console.print('❌ [red]We had a problem in the pipeline! Make sure you\'re in the same path of the image you want to convert or the Path is correct! [/]')
-        sys.exit(0)
-    console.print('🔨 [yellow]manufacturing your gruvbox wallpaper...[/]')
-    
+    return [
+        os.path.expanduser(path)
+        for path in console.input(
+            "🖼️ [bold yellow]Which image(s) do you want to manufacture? (image paths separated by spaces):[/] "
+        ).split()
+    ]
 
-    #TODO: might be a better idea to save the new Image in the same directory the command is being run from
-    dirname = os.path.join(os.path.dirname(image_file),'') # Gets the dirname of the file and appends a trailing slash if needed
-    filename = os.path.basename(image_file) # Gets the name of the file
 
-    # Resulting save_path
-    save_path =  dirname + 'gruvbox_' + filename
+def process_image(image_path, console, gruvbox_factory):
+    image = gruvbox_factory.open_image(image_path)
+
+    console.print(f"🔨 [yellow]manufacturing '{os.path.basename(image_path)}'...[/]")
+
+    # TODO: might be a better idea to save the new Image in the same directory the command is being run from
+    save_path = os.path.join(
+        os.path.dirname(image_path), "gruvbox_" + os.path.basename(image_path)
+    )
 
     gruvbox_factory.convert_image(image, save_path=(save_path))
-    console.print('✅ [bold green]Done![/] [green](saved as ' + save_path + ')[/]')
+    console.print(f"✅ [bold green]Done![/] [green](saved at '{save_path}')[/]")
 
 
 def add_gruvbox_palette(gruvbox_factory):
-    current_path = Path(__file__).parent.absolute()
-    palette = open(str(current_path) + '/gruvbox.txt', 'r')
-    for line in palette.readlines():
-        gruvbox_factory.add_color_to_palette(line[:-1])
+    palette_path = os.path.join(os.getcwd(), "gruvbox.txt")
+
+    with open(palette_path, "r") as f:
+        for line in f.readlines():
+            gruvbox_factory.add_color_to_palette(line[:-1])
+
 
 ## handle CTRL + C
 def signal_handler(signal, frame):
     print()
     sys.exit(0)
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     main()
