21f045c00d486ca770f5fe17ba10964de5470b6d
JoshuaDRose <joshuarose099@gmail.com>
2023-10-30 13:29:34 +1100

    feat: add wildcard support

--- factory/__main__.py
+++ factory/__main__.py
@@ -1,13 +1,15 @@
-import signal
 import argparse
-import sys
 import os
+import signal
+import sys
+from os import listdir
+from os.path import *
 from pathlib import Path
 
 from ImageGoNord import GoNord
-
 from rich.console import Console
 from rich.panel import Panel
+
 
 def main():
     signal.signal(signal.SIGINT, signal_handler)
@@ -28,13 +30,25 @@ def main():
         add_gruvbox_palette(gruvbox_factory, palette)
 
     for image_path in image_paths:
-        if os.path.isfile(image_path):
-            process_image(image_path, console, gruvbox_factory)
-        else:
+        folder, file = split(image_path)
+        if file == "*":
+            # assert that image_path is a folder that can be iterated through
+            if isdir(join(dirname(split(image_path)[0]), split(folder)[1])):
+                for i in listdir(folder):
+                    # TODO: make a check to see that 'i' ends with png, jpg, etc ...
+                    process_image(i, console, gruvbox_factory)
+                continue
+            else:
+                console.print(
+                    f"❌ [red]We had a problem in the pipeline! \n'{split(folder)[1]}' is not a folder that can be iterated through! \nSkipping... [/]"
+                )
+        elif not isfile(image_path):
             console.print(
                 f"❌ [red]We had a problem in the pipeline! \nThe image at '{image_path}' could not be found! \nSkipping... [/]"
             )
             continue
+        process_image(image_path, console, gruvbox_factory)
+
 
 # Gets the file path from the Argument
 def fromCommandArgument(console):
@@ -42,11 +56,22 @@ def fromCommandArgument(console):
         description="A simple cli to manufacture gruvbox themed wallpapers."
     )
 
-    command_parser.add_argument("-p", "--palette", choices=["white", "pink"], nargs="?", default="pink", type=str, help="choose your palette, panther 'pink' (default) or snoopy 'white'")
-    command_parser.add_argument("-i", "--images", nargs="+", type=str, help="path(s) to the image(s).")
+    command_parser.add_argument(
+        "-p",
+        "--palette",
+        choices=["white", "pink"],
+        nargs="?",
+        default="pink",
+        type=str,
+        help="choose your palette, panther 'pink' (default) or snoopy 'white'",
+    )
+    command_parser.add_argument(
+        "-i", "--images", nargs="+", type=str, help="path(s) to the image(s)."
+    )
 
     args = command_parser.parse_args()
     return args
+
 
 # Gets the file path from user input
 def fromTui(console):
@@ -57,44 +82,55 @@ def fromTui(console):
     )
 
     image_paths = [
-        os.path.expanduser(path) for path in console.input(
+        expanduser(path)
+        for path in console.input(
             "🖼️ [bold yellow]Which image(s) do you want to manufacture? (image paths separated by spaces):[/] "
         ).split()
     ]
-    palette = console.input("🎨 [bold yellow]Which palette do you prefer? (panther 'pink' or snoopy 'white'):[/] ")
-    if palette not in ['pink', 'white']:
+    palette = console.input(
+        "🎨 [bold yellow]Which palette do you prefer? (panther 'pink' or snoopy 'white'):[/] "
+    )
+    if palette not in ["pink", "white"]:
         console.print(
-                f"❌ [red]We had a problem in the pipeline! \nThe palette you chose is not available! \nShuting down... [/]"
-            )
+            f"❌ [red]We had a problem in the pipeline! \nThe palette you chose is not available! \nShuting down... [/]"
+        )
         palette = None
 
     return (image_paths, palette)
 
+
 def process_image(image_path, console, gruvbox_factory):
     image = gruvbox_factory.open_image(image_path)
 
-    console.print(f"🔨 [yellow]manufacturing '{os.path.basename(image_path)}'...[/]")
+    console.print(f"🔨 [yellow]manufacturing '{basename(image_path)}'...[/]")
 
     # TODO: might be a better idea to save the new Image in the same directory the command is being run from
-    save_path = os.path.join(
-        os.path.dirname(image_path), "gruvbox_" + os.path.basename(image_path)
-    )
+    # NOTE: this is done with the following code:
+    """
+    >>> save_path = os.path.join(
+    >>>     os.getcwd(), "gruvbox_" + os.path.basename(image_path)
+    >>> )
+    """
+    save_path = join(dirname(image_path), "gruvbox_" + basename(image_path))
 
     gruvbox_factory.convert_image(image, save_path=(save_path))
     console.print(f"✅ [bold green]Done![/] [green](saved at '{save_path}')[/]")
+
 
 def add_gruvbox_palette(gruvbox_factory, palette):
     current_path = Path(__file__).parent.absolute()
     file_name = f"gruvbox-{palette}.txt"
 
-    with open(os.path.join(current_path, file_name), "r") as f:
+    with open(join(current_path, file_name), "r") as f:
         for line in f.readlines():
             gruvbox_factory.add_color_to_palette(line[:-1])
+
 
 ## handle CTRL + C
 def signal_handler(signal, frame):
     print()
     sys.exit(0)
+
 
 if __name__ == "__main__":
     main()
